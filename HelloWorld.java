0d09b2faed8b654509b065d5bea3bf19be94044e
binoy2119 <chhatwanibinoy@gmail.com>
2024-10-23 00:44:56 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // This is a comment
+        // The following line will print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
